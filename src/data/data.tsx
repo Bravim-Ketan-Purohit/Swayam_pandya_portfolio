0292374f4c4d87edf7959bed3f229dd206969a9b
Swayam Pandya <swayampandya1236@gmail.com>
2023-06-23 13:38:27 +0530

    title update

--- src/data/data.tsx
+++ src/data/data.tsx
@@ -47,8 +47,8 @@ import {
  * Page meta data
  */
 export const homePageMeta: HomepageMeta = {
-  title: 'React Resume Template',
-  description: "Example site built with Tim Baker's react resume template",
+  title: 'Swayam Pandya',
+  description: "My portfolio webpage",
 };
 
 /**
